d3478655826152c65c52dc0d606ac3b74a9fe676
ardilalukita14 <lukitasari623@gmail.com>
2021-02-20 07:31:13 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return "Nama : Ardila Lukita Sari <br>NIM : 1941720043";
+    }
+}
